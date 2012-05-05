aa3162ddd327b64eb8e548aae7302de5d8042c17
Dominick Pham <dominick.pham@gmail.com>
2012-05-04 22:55:04 -0400

    collision

--- main.py
+++ main.py
@@ -3,16 +3,22 @@ import os
 import math
 import pyglet
 import cocos
+
 from cocos.director import director
 from cocos.sprite import Sprite
 
-from cocos.actions.move_actions import WrappedMove
-from cocos.actions.move_actions import BoundedMove
-from cocos.actions.move_actions import Driver
+import cocos.euclid as eu
+import cocos.collision_model as cm
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
-
+class CollidableRectSprite(cocos.sprite.Sprite):
+    def __init__(self, image, center_x, center_y):
+        super(CollidableRectSprite, self).__init__(image)
+        self.position = (center_x, center_y)
+        self.vector2 = eu.Vector2(center_x, center_y)
+        self.cshape = cm.AARectShape(self.vector2, self.width/2, self.height/2)
+        print(self.width, self.height, self.cshape, self.vector2)
 
 # Movements
 class BounceBoundedMove(cocos.actions.move_actions.Move):
@@ -31,19 +37,23 @@ class BounceBoundedMove(cocos.actions.move_actions.Move):
         pos = self.target.position
         vel = self.target.velocity
 
-
         if pos[0] < self.half_width and vel[0] < 0:
             vel = math.fabs(vel[0]), vel[1]
+            if "collide_left" in vars(self): self.collide_left()
         elif pos[0] > self.width - self.half_width and vel[0] > 0:
             vel = -math.fabs(vel[0]), vel[1]
+            if "collide_right" in vars(self): self.collide_right()
 
         if pos[1] < self.offsetY + self.half_height and vel[1] < 0:
             vel = vel[0], math.fabs(vel[1])
+            if "collide_bottom" in vars(self): self.collide_bottom()
         elif pos[1] > self.height - self.half_height and vel[1] > 0:
             vel = vel[0], -math.fabs(vel[1])
+            if "collide_top" in vars(self): self.collide_top()
 
         self.target.velocity = vel
         self.target.position = pos[0] + vel[0]*dt, pos[1] + vel[1]*dt
+
 
 
 class Monster(cocos.layer.Layer):
@@ -65,14 +75,12 @@ class Paddle(object):
         self.y = fixedY
         self.offsetX = 76;
 
-        self.sprite = Sprite("images/paddle.png")
+        self.sprite = CollidableRectSprite("images/paddle.png", 100, 400)
 
     def setPosition(self, x, scene_width):
         width = self.sprite.width
-        if x < width:
-            x = width
-        elif x > scene_width:
-            x = scene_width
+        if x < width: x = width
+        elif x > scene_width: x = scene_width
         self.sprite.position = x-width/2, self.y
 
 
@@ -85,20 +93,35 @@ class GameScene(cocos.layer.Layer):
         self.dimension = 616
         self.position = 76, 348
 
+
         track_sprite = Sprite('images/track.png')
         track_sprite.position = self.dimension/2, 27
         self.add( track_sprite, z=1 )
 
-        self.paddle = Paddle(27)
-        self.paddle.setPosition(self.dimension/2, self.dimension)
-        self.add( self.paddle.sprite, z=2)
-
-        ball_movement = BounceBoundedMove(self.dimension, self.dimension, 30)
-        self.ball = Sprite("images/ball.png")
+        # ball and movement
+        ball_movement = BounceBoundedMove(self.dimension, self.dimension, 10)
+        ball_movement.collide_bottom = self.ball_collide_bottom
+        self.ball = CollidableRectSprite("images/ball.png", 100, 400)
         self.add(self.ball, z=10)
         self.ball.velocity = (200, 136)
         self.ball.do(ball_movement)
 
+
+        # paddle
+        self.paddle = Paddle(27)
+        self.paddle.setPosition(self.dimension/2, self.dimension)
+        self.add( self.paddle.sprite, z=2)
+
+
+        collision = cm.CollisionManager();
+        collision.clear()
+        collision.add(self.paddle.sprite)
+        collision.add(self.ball)
+        print(collision.known_objs(), collision.any_near(100, 400))
+
+    def ball_collide_bottom(self):
+        print ("hit bottom")
+        #print(self.collision.known_objs(), self.collision.they_collide(self.ball, self.paddle.sprite))
 
     def on_mouse_motion (self, x, y, dx, dy):
         self.paddle.setPosition(x, self.dimension)
