5e14ae6a3fd1bab125e9cce9f116fbdcbcd01a9f
Dominick Pham <dominick.pham@gmail.com>
2012-05-04 20:55:59 -0400

    move paddle and ball

--- main.py
+++ main.py
@@ -1,26 +1,54 @@
 import sys
 import os
+import math
 import pyglet
 import cocos
 from cocos.director import director
 from cocos.sprite import Sprite
 
+from cocos.actions.move_actions import WrappedMove
+from cocos.actions.move_actions import BoundedMove
+from cocos.actions.move_actions import Driver
+
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
-class Trajectory():
-    def __init__(Self):
-        print("log")
-
-    def start():
-        print("log")
-
-    def stop():
-        print("log")
 
 
-class Enemy(cocos.layer.Layer):
+# Movements
+class BounceBoundedMove(cocos.actions.move_actions.Move):
+    def __init__(self, width, height, offsetY):
+        super(BounceBoundedMove, self).__init__()
+        self.width = width
+        self.height = height
+        self.offsetY = offsetY
+
+    def start(self):
+        self.target.position = self.width/2, self.height/2
+        self.half_width = self.target.width/2
+        self.half_height = self.target.height/2
+
+    def step(self, dt):
+        pos = self.target.position
+        vel = self.target.velocity
+
+
+        if pos[0] < self.half_width and vel[0] < 0:
+            vel = math.fabs(vel[0]), vel[1]
+        elif pos[0] > self.width - self.half_width and vel[0] > 0:
+            vel = -math.fabs(vel[0]), vel[1]
+
+        if pos[1] < self.offsetY + self.half_height and vel[1] < 0:
+            vel = vel[0], math.fabs(vel[1])
+        elif pos[1] > self.height - self.half_height and vel[1] > 0:
+            vel = vel[0], -math.fabs(vel[1])
+
+        self.target.velocity = vel
+        self.target.position = pos[0] + vel[0]*dt, pos[1] + vel[1]*dt
+
+
+class Monster(cocos.layer.Layer):
     def __init__(self):
-        super( Enemy, self ).__init__()
+        super( Monster, self ).__init__()
 
         self.ww = director.window.width
         self.wh = director.window.height
@@ -30,32 +58,26 @@ class Enemy(cocos.layer.Layer):
         self.add( sprite, z=1 )
 
 
-class Paddle(cocos.layer.Layer):
-    is_event_handler = True
+class Paddle(object):
     def __init__(self, fixedY):
         super(Paddle, self).__init__()
 
         self.y = fixedY
         self.offsetX = 76;
-        self.width = 159
 
         self.sprite = Sprite("images/paddle.png")
-        self.add( self.sprite)
-        self.sprite.position = 0, 0
 
-    def setX(self, x):
-        self.position = x, self.y
-
-    def on_mouse_motion (self, x, y, dx, dy):
-        x = x-self.width/2
-        if x < self.width/2:
-            x = self.width/2
-        elif x > 616-self.width/2:
-            x = 616-self.width/2
-        self.position = x, self.y
+    def setPosition(self, x, scene_width):
+        width = self.sprite.width
+        if x < width:
+            x = width
+        elif x > scene_width:
+            x = scene_width
+        self.sprite.position = x-width/2, self.y
 
 
 class GameScene(cocos.layer.Layer):
+    is_event_handler = True
     def __init__(self):
         super(GameScene, self).__init__()
 
@@ -68,8 +90,22 @@ class GameScene(cocos.layer.Layer):
         self.add( track_sprite, z=1 )
 
         self.paddle = Paddle(27)
-        self.paddle.setX(self.dimension/2)
-        self.add( self.paddle, z=2)
+        self.paddle.setPosition(self.dimension/2, self.dimension)
+        self.add( self.paddle.sprite, z=2)
+
+        ball_movement = BounceBoundedMove(self.dimension, self.dimension, 30)
+        self.ball = Sprite("images/ball.png")
+        self.add(self.ball, z=10)
+        self.ball.velocity = (200, 136)
+        self.ball.do(ball_movement)
+
+
+    def on_mouse_motion (self, x, y, dx, dy):
+        self.paddle.setPosition(x, self.dimension)
+
+
+
+
 
 
 
@@ -95,5 +131,7 @@ if __name__ == "__main__":
     # director init takes the same arguments as pyglet.window
     director.init( width=768, height=1024, caption="Pong Attack" )
 
+    game_scene = GameScene()
     # And now, start the application, starting with main_scene
-    director.run (cocos.scene.Scene (BackgroundLayer(), GameScene(), ForegroundLayer()))
+    director.run (cocos.scene.Scene (BackgroundLayer(), game_scene, ForegroundLayer()))
+
